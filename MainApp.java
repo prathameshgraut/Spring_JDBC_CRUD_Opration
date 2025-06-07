a621380418fd13ac3bde54a015377962a94a58c6
Prathamesh Raut. <prathamesh1687@gmail.com>
2025-06-07 09:53:32 +0530

    Update MainApp.java

--- MainApp.java
+++ MainApp.java
@@ -14,43 +14,43 @@ public class MainApp {
 		JdbcTemplate JT = ref.getBean(JdbcTemplate.class);
 
 		
-//		int Id=115;
-//		String Name="Vivo";
-//		double Price=27599;
-//		String Model="V29 Pro";
-//		String Processor="Snapdragon 512";
-//		
+		int Id=115;
+		String Name="Vivo";
+		double Price=27599;
+		String Model="V29 Pro";
+		String Processor="Snapdragon 512";
 //		//insertion operation
-//		String query="insert into Mobile_Shop values(?,?,?,?,?)";
-//		int Add = JT.update(query,Id,Name,Price,Model,Processor);
-//		if(Add>0) {
-//			System.out.println("Data Added SuccessFully..!!!");
-//		}else {
-//			System.out.println("Failed,Data Not Added ");
-//		}
+		String query="insert into Mobile_Shop values(?,?,?,?,?)";
+		int Add = JT.update(query,Id,Name,Price,Model,Processor);
+		if(Add>0) {
+			System.out.println("Data Added SuccessFully..!!!");
+		}else {
+			System.out.println("Failed,Data Not Added ");
+		}
+
 		
-//		int Id=114;
-//		String Processor="Oled HD+";
-//		
+		int Id=114;
+		String Processor="Oled HD+";
 //		//update opertion
-//		String Query="update Mobile_Shop set Processor=? where Id=?";
-//		int Update=JT.update(Query,Processor,Id);
-//		if(Update>0) {
-//			System.out.println("Data Updated SuccessFully....");
-//		}else {
-//			System.out.println("Due To The Some Error");
-//		}
+		String Query="update Mobile_Shop set Processor=? where Id=?";
+		int Update=JT.update(Query,Processor,Id);
+		if(Update>0) {
+			System.out.println("Data Updated SuccessFully....");
+		}else {
+			System.out.println("Due To The Some Error");
+		}
+
 		
-//		int Id=115;
-//		
+		int Id=115;
 //		//Drop data operation
-//		String Query="delete from Mobile_Shop where Id=?";
-//		int del=JT.update(Query,Id);
-//		if(del>0) {
-//			System.out.println("Deleted Data SuccessFully!!");
-//		}else {
-//			System.out.println("Failed Not Deleted Data");
-//		}
+		String Query="delete from Mobile_Shop where Id=?";
+		int del=JT.update(Query,Id);
+		if(del>0) {
+			System.out.println("Deleted Data SuccessFully!!");
+		}else {
+			System.out.println("Failed Not Deleted Data");
+		}
+
 		
 		// Show Operation
 		String Query="select * from Mobile_Shop";
